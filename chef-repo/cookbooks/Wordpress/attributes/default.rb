0ff1435dddf44e6b8ca592e1e106cee1c3257404
Andres Olarte <78939028+AndresOlarteZabala@users.noreply.github.com>
2023-05-09 18:38:16 -0500

    Change apache document root folder path

--- chef-repo/cookbooks/Wordpress/attributes/default.rb
+++ chef-repo/cookbooks/Wordpress/attributes/default.rb
@@ -17,7 +17,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-default['apache']['document_root'] = "/var/www/html/blog"
+default['apache']['document_root'] = "/var/www/html"
 # General settings
 default['wordpress']['version'] = "3.1.2"
 default['wordpress']['checksum'] = "1006a1bb97b42381ad82490d00d9b7fb9f7a1c9d83ee2ed36935a9eb99c81064"
