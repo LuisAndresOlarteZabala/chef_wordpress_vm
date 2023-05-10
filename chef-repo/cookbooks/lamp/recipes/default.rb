ce2428cd64ebebba423485fbd57afeacf01749b9
Fernando Contreras Pardo <fc7430@gmail.com>
2023-05-09 23:53:06 -0500

    Unit test

--- chef-repo/cookbooks/lamp/recipes/default.rb
+++ chef-repo/cookbooks/lamp/recipes/default.rb
@@ -9,7 +9,6 @@
 #include_recipe '::facts'
 
   begin
-    include_recipe "::lamp_#{node['lamp']['family']}" rescue 
+    include_recipe "lamp::lamp_#{node['lamp']['family']}" rescue 
     Chef::Exceptions::RecipeNotFound
-    Chef::Log.warn"A #{node['lamp']['family']} recipe does not exist for the PLATFORM FAMILY: #{node['platform_family']}"
   end
---
+++ chef-repo/cookbooks/lamp/spec/unit/recipes/lamp_ubuntu_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+#Podemos también generar dinámicamente los casos de prueba para múltiples plataformas utilizando código Ruby. En este ejemplo se itera por la lista de plataformas y versiones, y se generan los casos de prueba dinámicamente en función de ellos, cada uno define las expectativas de los paquetes necesarios que se instalarán.
+
+# Echar un ojo a https://github.com/chefspec/fauxhai/blob/main/PLATFORMS.md
+# para que que versiones son soportadas. Por ejemplo: Debian 8.11 no, y salta error
+#ChefSpec::SoloRunner.new  => hace que dinamicamente todo sea  node.normal['vim']['install_method'] = 'package'
+# a ver si existe ese metodo de instalacion
+
+describe 'lamp::lamp_ubuntu' do
+  package_checks = {
+    'ubuntu' => {
+      '16.04' => ['apache2','mysql-server','php-mysql'],
+      '18.04' => ['apache2','mysql-server','php-mysql'],
+      '20.04' => ['apache2','mysql-server','php-mysql'],
+    }
+  }
+  
+  package_checks.each do |platform, versions|
+    versions.each do |version, packages|
+      packages.each do |package_name|
+        it "should install #{package_name} on #{platform} #{version}" do
+          chef_run = ChefSpec::SoloRunner.new(platform: platform, version: version, file_cache_path: '/var/chef/cache') do |node|
+            node.normal['lamp']['family'] = "ubuntu"
+          end.converge(described_recipe)
+         # Chef::Log.warn" package_name = #{package_name}"
+          expect(chef_run).to install_package(package_name)
+        end
+      end
+    end
+  end
+end
